f4d7e1408dd99e7d6f64958dd826fac23fb58859
Joshua Kleveter <joshua.kleveter@gmail.com>
2016-01-01 11:28:26 -0600

    Moved routing functions to router.js

--- app.js
+++ app.js
@@ -16,27 +16,3 @@ http.createServer(function serverHandler(request, response) {
 }).listen(port, function listenHandler() {
     process.stdout.write("Server running at http://localhost:3000\n");
 });
-
-
-
-////////////
-//Routing //
-////////////
-
-function homeRoute(request, response) {
-    if(request.url === "/") {
-        response.writeHead(200, {"Content-Type": "text/plain"});
-        response.write("Header\n");
-        response.write("Search\n");
-        response.end("Footer\n");
-    }
-}
-
-
-function userRoute(request, response) {
-    var username = request.url.replace("/", "");
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Header\n");
-    response.write(username + "\n");
-    response.end("Footer\n");
-}
---
+++ router.js
@@ -0,0 +1,21 @@
+////////////////
+//App Routing //
+////////////////
+
+function homeRoute(request, response) {
+    if(request.url === "/") {
+        response.writeHead(200, {"Content-Type": "text/plain"});
+        response.write("Header\n");
+        response.write("Search\n");
+        response.end("Footer\n");
+    }
+}
+
+
+function userRoute(request, response) {
+    var username = request.url.replace("/", "");
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("Header\n");
+    response.write(username + "\n");
+    response.end("Footer\n");
+}
