0b40abb1bfc5c2af17f855152e38652ab26e38dc
Joshua Kleveter <joshua.kleveter@gmail.com>
2016-01-01 13:24:51 -0600

    Wrote renderTemplate() function

--- renderer.js
+++ renderer.js
@@ -10,9 +10,23 @@ var fs = require("fs");
 //Template Renderer Module //
 /////////////////////////////
 
+
+function renderTemplate(values, content) {
+
+    for (var key in values) {
+        var regExp = new RegExp("\\{{2}\\s*" + key + "\\s*\\}{2}", "i");
+        content = content.replace(regExp, values[key]);
+    }
+
+    return content;
+
+}
+
+
 function view(templateName, values, response) {
 
-    var fileContents = fs.readFileSync("./views/" + templateName + ".html");
+    var fileContents = fs.readFileSync("./views/" + templateName + ".html", "utf8");
+    fileContents = renderTemplate(values, fileContents);
     response.write(fileContents);
 
 }
