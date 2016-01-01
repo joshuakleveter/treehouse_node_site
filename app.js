ccd873c64c97b3ea0cede46918c998d9da98d83b
Joshua Kleveter <joshua.kleveter@gmail.com>
2016-01-01 11:27:20 -0600

    Wrote basic userRoute() function

--- app.js
+++ app.js
@@ -31,3 +31,12 @@ function homeRoute(request, response) {
         response.end("Footer\n");
     }
 }
+
+
+function userRoute(request, response) {
+    var username = request.url.replace("/", "");
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("Header\n");
+    response.write(username + "\n");
+    response.end("Footer\n");
+}
