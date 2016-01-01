980532aa26da8e5955fee69e700381477a43a5e8
Joshua Kleveter <joshua.kleveter@gmail.com>
2016-01-01 11:10:15 -0600

    Added doc blocks

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
+////////////
+//Imports //
+////////////
+
 var http = require("http");
 
-var port = 3000;
 
+
+///////////////////
+//Node.js Server //
+///////////////////
+
+var port = 3000;
 http.createServer(function serverHandler(request, response) {
   response.writeHead(200, { "Content-Type": "text/plain" });
   response.end("Hello World\n");
 }).listen(port, function listenHandler() {
   console.log("Server running at http://localhost:3000");
 });
+
+
+
+////////////
+//Routing //
+////////////
