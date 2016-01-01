662cb5f258ee7c6a7b22305091f89b0f425b2a92
Joshua Kleveter <joshua.kleveter@gmail.com>
2016-01-01 12:10:06 -0600

    Parse student profile

--- router.js
+++ router.js
@@ -1,3 +1,11 @@
+////////////
+//Imports //
+////////////
+
+var Profile = require("./profile.js");
+
+
+
 ////////////////
 //App Routing //
 ////////////////
@@ -17,8 +25,22 @@ function user(request, response) {
     if(username.length > 0) {
         response.writeHead(200, {"Content-Type": "text/plain"});
         response.write("Header\n");
-        response.write(username + "\n");
-        response.end("Footer\n");
+
+        var studentProfile = new Profile(username);
+        studentProfile.on("end", function parseProfileJSON(profileJSON) {
+            var profileData = {
+                avatarURL: profileJSON.gravatar_url,
+                username: profileJSON.profile_name,
+                badgeCount: profileJSON.badges.length,
+                javaScriptPoints: profileJSON.points.JavaScript
+            };
+            response.write(profileData.username + " has " + profileData.badgeCount + " badges\n");
+            response.end("Footer\n");
+        });
+        studentProfile.on("error", function profileErrorHandler(error) {
+            response.write(error.message + "\n");
+            response.end("Footer\n");
+        });
     }
 }
 
